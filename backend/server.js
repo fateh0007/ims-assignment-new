e8c46e7078c110568fd13dd17750daa28e64db1b
fateh0007 <fatehalam123463@gmail.com>
2025-12-10 21:26:20 +0530

    Backend home route change

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,12 @@ app.use('/api/summarize', require('./routes/summarize'));
 app.use('/api/payment', require('./routes/payment'));
 
 // Serve frontend in production (Vite build output)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html')));
-}
-
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/dist')));
+//   app.get('*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html')));
+// }
+app.get("/",(req,res)=>{
+  res.send("Hi");
+});
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, ()=> console.log('Server started on', PORT));
